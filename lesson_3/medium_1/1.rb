c125a2ee04aa00991b9864d558715203cd055010
Andrew  Pacheco <ndrewpacheco@gmail.com>
2020-05-02 18:30:29 -0400

    fin med 1

---
+++ lesson_3/medium_1/1.rb
@@ -0,0 +1,10 @@
+# For this practice problem, 
+#write a one-line program
+# that creates the following output 10 times,
+# with the subsequent line indented 1 space to the right:
+
+# The Flintstones Rock!
+#  The Flintstones Rock!
+#   The Flintstones Rock!
+
+10.times {| n| puts ' ' * n  + "The Flintstones Rock!" }#+ "The Flintstones Rock!"}
---
+++ lesson_3/medium_1/3.rb
@@ -0,0 +1,32 @@
+# Question 3
+# Alan wrote the following method, 
+#which was intended to show all of the factors of the input number:
+
+def factors(number)
+  divisor = number
+  factors = []
+
+
+  while divisor > 0 do 
+    factors << number / divisor if number % divisor == 0
+    divisor -= 1 
+  end 
+  factors
+end
+
+p factors(12)
+p factors(0)
+p factors(-12)
+# Alyssa noticed that this will fail if the input is 0, or a negative number, 
+#and asked Alan to change the loop. 
+#How can you make this work without using begin/end/until? 
+# Note that we're not looking to find the factors for 0 or negative numbers, 
+#but we just want to handle it gracefully instead of raising an exception or going into an infinite loop.
+
+# Bonus 1
+# What is the purpose of the number % divisor == 0 ?
+
+# Bonus 2
+# What is the purpose of the second-to-last line (line 8) in the method (the factors before the method's end)?
+
+# Solution 3
