242012e5e01a637d8d635ec67709f9a6947c1ca5
Andrew  Pacheco <ndrewpacheco@gmail.com>
2020-05-14 22:38:26 -0400

    fin lesson 5

--- lesson_5/quiz.rb
+++ lesson_5/quiz.rb
@@ -6,7 +6,8 @@
 # 6 D 6
 # 7 C 7
 # 8 BC n
-# 9
+# 9 ABC 8 
+# 10 C 9
 
 order_data = [
   {customer_id: 12, customer_name: 'Emma Lopez', order_id: 351, order_date: '12/04/16', order_fulfilled: true, order_value: 135.99},
@@ -38,20 +39,30 @@ customer_orders = [
   # rest of data...
 ]
 
+all_orders =[
+  {customer_id: 12, customer_name: 'Emma Lopez', total_order_value: 483.48},
+  {customer_id: 32, customer_name: 'Michael Richards', total_order_value: 205.65},
+  # rest of data
+]
 
-temp_orders = {}
+fulfilled_orders =[
+  {customer_id: 12, customer_name: 'Emma Lopez', order_value: 425.48},
+  {customer_id: 32, customer_name: 'Michael Richards', order_value: 120.00},
+  # rest of data
+]
 
-order_data.each do |row|
-  temp_orders[row[:customer_id]] = {
-      customer_name: row[:customer_name],
-      customer_id: row[:customer_id],
-      orders: [
-        {
-          order_fulfilled: row[:order_fulfilled],
-          order_value: row[:order_value]
-        }
-      ]
-    }
+############
+fulfilled_orders = customer_orders.map do |customer|
+  {
+    customer_id: customer[:customer_id],
+    customer_name: customer[:customer_name]
+  }
 end
 
-customer_orders = temp_orders.values 
+customer_orders.each_with_index do |data, index|
+  order_value = data[:orders].reduce(0) do |total, order|
+    total + order[:order_value] if order[:order_fulfilled]
+  end
+
+  fulfilled_orders[index][:order_value] = order_value
+end
