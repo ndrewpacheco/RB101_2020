f3d3bfa1652b228a0e196549da4a61fc67cf9e54
Andrew  Pacheco <ndrewpacheco@gmail.com>
2020-05-16 14:55:07 -0400

    working on tictac

--- lesson_6/ttt.rb
+++ lesson_6/ttt.rb
@@ -1,3 +1,10 @@
+INITIAL_MARKER = " "
+PLAYER_MARKER = "X"
+COMPUTER_MARKER = "O"
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
 def display_board(brd)
   puts ""
   puts "     |     |"
@@ -16,10 +23,34 @@ end
 def init_board
   new_brd = {}
 
-  (1..9).each {|num| new_brd[num] = " "}
+  (1..9).each {|num| new_brd[num] = INITIAL_MARKER}
   new_brd
 end
 
-board = init_board
+def empty_squares(brd)
+  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
+end
 
+def player_places_piece!(brd)
+  square = ""
+  loop do 
+    prompt "Choose a square (#{empty_squares(brd).join(", ")}):"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt "Not a valid answer."
+  end
+  brd[square] = PLAYER_MARKER
+end
+
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+board = init_board
 display_board(board)
+
+player_places_piece!(board)
+computer_places_piece!(board)
+display_board(board)
+
