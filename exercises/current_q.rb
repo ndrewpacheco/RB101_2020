3e8b54a252f1aae48750bbe7dc01bbf25c9afd40
Andrew  Pacheco <ndrewpacheco@gmail.com>
2020-05-30 20:00:05 -0400

    refreshing exercises

--- exercises/current_q.rb
+++ exercises/current_q.rb
@@ -1,56 +1,50 @@
+# Letter Counter (Part 1)
+# Write a method that takes a string with one or more space separated words
+# and returns a hash that shows the number of words of different sizes.
 
-MINUTES_IN_A_DAY = 1440
-MINUTES_IN_AN_HOUR = 60
+# Words consist of any string of characters that do not include a space.
 
-def time_of_day(num)
+# Examples
 
-  num += MINUTES_IN_A_DAY if num < 0
-  num %= MINUTES_IN_A_DAY if num > MINUTES_IN_A_DAY || num * -1 > MINUTES_IN_A_DAY
 
-  hrs = num / MINUTES_IN_AN_HOUR
-  mins =  num - ( hrs * MINUTES_IN_AN_HOUR )
+# sort through string
+# take the size of each str
+#hsh if else statement
 
-  "#{format("%.2d", hrs)}:#{format("%.2d", mins)}"
+#
+def word_sizes(str)
+  str = delete_nonalpha(str)
+  hsh = {}
 
+  str.split.each do |word|
+
+
+    if hsh[word.size].nil?
+      hsh[word.size] = 1
+    else
+      hsh[word.size] += 1
+    end
+  end
+  hsh
+end
+# Modify the word_sizes method from the previous exercise
+#to exclude non-letters when determining word size.
+#For instance, the length of "it's" is 3, not 4.
+
+# Examples
+
+# seperate method that deletes non alpha chars
+
+def delete_nonalpha(str)
+  str.split.map do |word|
+    word.chars.select do |char|
+      ("A".."Z").include?(char.upcase)
+    end.join
+  end.join(' ')
 end
 
-def after_midnight(time)
-   # "#{format("%.2d", hrs)}:#{format("%.2d", mins)}" #### "00:00"
-  hours = time[0,2].to_i
-  mins = time[3,2].to_i
-  total_mins = ( hours * MINUTES_IN_AN_HOUR ) + mins
 
-
-  total_mins % MINUTES_IN_A_DAY
-
-end
-
-def before_midnight(time)
-  hours = time[0,2].to_i
-  mins = time[3,2].to_i
-  total_mins = ( hours * MINUTES_IN_AN_HOUR ) + mins
-
-  total_mins %= MINUTES_IN_A_DAY
-  return total_mins if total_mins == 0
-
-  MINUTES_IN_A_DAY - total_mins
-  #mins
-end
-
-
-# p time_of_day(0)== "00:00"
-# p time_of_day(-3) == "23:57"
-# p time_of_day(35) == "00:35"
-# p time_of_day(-1437) == "00:03"
-# p time_of_day(3000) == "02:00"
-# p time_of_day(800) == "13:20"
-# p time_of_day(-4231) == "01:29"
-
-
-
-p after_midnight('00:00') == 0
-p before_midnight('00:00') == 0
-p after_midnight('12:34') == 754
-p before_midnight('12:34') == 686
-p after_midnight('24:00') == 0
-p before_midnight('24:00') == 0
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
+p word_sizes('') == {}
