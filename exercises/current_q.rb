c5e69a6dcde2ed2ccb561d6410b077a8f50b59a8
Andrew  Pacheco <ndrewpacheco@gmail.com>
2020-06-01 23:02:45 -0400

    working on exeriise

--- exercises/current_q.rb
+++ exercises/current_q.rb
@@ -1,50 +1,25 @@
-# Letter Counter (Part 1)
-# Write a method that takes a string with one or more space separated words
-# and returns a hash that shows the number of words of different sizes.
+# Write a method that takes an Array of Integers between 0 and 19,
+# and returns an Array of those Integers sorted based on the English words
+# for each number:
 
-# Words consist of any string of characters that do not include a space.
+# zero, one, two, three, four, five, six, seven, eight, nine, ten,
+# eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen
 
-# Examples
-
-
-# sort through string
-# take the size of each str
-#hsh if else statement
-
-#
-def word_sizes(str)
-  str = delete_nonalpha(str)
-  hsh = {}
-
-  str.split.each do |word|
+# Examples:
 
 
-    if hsh[word.size].nil?
-      hsh[word.size] = 1
-    else
-      hsh[word.size] += 1
-    end
-  end
-  hsh
+NUMS = %w(zero, one, two, three, four, five, six, seven, eight, nine, ten,
+eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen)
+
+
+def alphabetic_number_sort(arr)
+
+  new_arr = arr.map {|num| NUMS[num]}.sort
+
+  new_arr.map {|num| NUMS.index(num)}
+
 end
-# Modify the word_sizes method from the previous exercise
-#to exclude non-letters when determining word size.
-#For instance, the length of "it's" is 3, not 4.
-
-# Examples
-
-# seperate method that deletes non alpha chars
-
-def delete_nonalpha(str)
-  str.split.map do |word|
-    word.chars.select do |char|
-      ("A".."Z").include?(char.upcase)
-    end.join
-  end.join(' ')
-end
-
-
-p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
-p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
-p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
-p word_sizes('') == {}
+p alphabetic_number_sort((0..19).to_a) == [
+  8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
+  6, 16, 10, 13, 3, 12, 2, 0
+]
