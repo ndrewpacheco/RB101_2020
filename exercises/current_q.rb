c14aca3df1b2dad7ec766d48706ef760da1640b7
Andrew  Pacheco <ndrewpacheco@gmail.com>
2020-06-23 14:44:20 -0400

    fin easy 6

--- exercises/current_q.rb
+++ exercises/current_q.rb
@@ -1,23 +1,30 @@
-# Write a method that takes an Array as an argument,
-# and returns two Arrays (as a pair of nested Arrays)
-# that contain the first half and second half of the original Array, respectively.
-# If the original array contains an odd number of elements,
-# the middle element should be placed in the first half Array.
+def triangle(num)
+  working_line = [" "] * num
 
-# Examples:
-
-
-def halvsies(arr)
-  arr_size = arr.size
-  midpoint = arr_size / 2
-  midpoint += 1 if arr_size.odd?
-
-  arr1 = arr[0, midpoint]
-  arr2 = arr[midpoint, midpoint]
-  [arr1, arr2]
+  num.times do |n|
+    working_line << "*"
+    working_line.shift
+    puts working_line.join
+  end
 end
 
-p halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
-p halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
-p halvsies([5]) == [[5], []]
-p halvsies([]) == [[], []]
+
+
+triangle(5)
+
+#     *
+#    **
+#   ***
+#  ****
+# *****
+triangle(9)
+
+#         *
+#        **
+#       ***
+#      ****
+#     *****
+#    ******
+#   *******
+#  ********
+# *********
