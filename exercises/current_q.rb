062f9e30039a3c9ec25a55333fe3d5ece38e2233
Andrew  Pacheco <ndrewpacheco@gmail.com>
2020-06-27 11:59:12 -0400

    fin fizzbuzz

--- exercises/current_q.rb
+++ exercises/current_q.rb
@@ -1,15 +1,30 @@
-# Write a method that returns the next to last word
-#in the String passed to it as an argument.
+# fizzbuzz
+# Write a method that takes two arguments:
+#the first is the starting number, and the second is the ending number.
+# Print out all numbers between the two numbers,
+# except if a number is divisible by 3, print "Fizz",
+# if a number is divisible by 5, print "Buzz",
+# and finally if a number is divisible by 3 and 5, print "FizzBuzz".
 
-# Words are any sequence of non-blank characters.
-
-# You may assume that the input String will always contain at least two words.
-
-# Examples:
+# Example:
 
 
-def penultimate(str)
-  str.split[-2]
+def fizzbuzz(num1, num2)
+
+  (num1..num2).to_a.map do |num|
+
+    if num % 3 == 0 && num % 5 == 0
+      "FizzBuzz"
+    elsif num % 5 == 0
+      "Buzz"
+    elsif num % 3 == 0
+      "Fizz"
+    else
+      num
+
+    end
+  end
+
 end
-p penultimate('last word') == 'last'
-p penultimate('Launch School is great!') == 'is'
+
+p fizzbuzz(1, 15) # -> 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
