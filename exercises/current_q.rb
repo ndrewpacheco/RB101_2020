ac4f4a087a7d554c046f19a4e9c730f21028efa7
Andrew  Pacheco <ndrewpacheco@gmail.com>
2020-07-18 17:10:00 -0400

    fin doubles

--- exercises/current_q.rb
+++ exercises/current_q.rb
@@ -1,26 +1,46 @@
-# Write a method that takes a non-empty string argument,
-# and returns the middle character or characters of the argument.
-# If the argument has an odd length, you should return exactly one character.
-# If the argument has an even length, you should return exactly two characters.
+# Double Doubles
+# A double number is a number with an even number of digits whose left-side digits
+# are exactly the same as its right-side digits.
+
+# For example, 44, 3333, 103103, 7676 are all double numbers.
+# 444, 334433, and 107 are not.
+
+# Write a method that returns 2 times the number provided as an argument,
+# unless the argument is a double number; double numbers should be returned as-is.
 
 # Examples:
 
-def center_of(str)
-  size = str.size
-  mid_char = size / 2
+def double_number?(num)
+  arr = num.to_s.chars
+  arr_size = arr.size
 
-  if size.odd?
-    #return mid_char with 1 character
-    str[mid_char]
-  else
-    #return mid_char with 2 characters
-  # return value middle char(s) of the arg
-    str[mid_char - 1, 2]
+  if arr_size.even?
+
+    arr2 = arr.slice!(0, arr.size / 2)
+    return true if arr == arr2
   end
+
+  false
 end
 
-p center_of('I love ruby') == 'e'
-p center_of('Launch School') == ' '
-p center_of('Launch') == 'un'
-p center_of('Launchschool') == 'hs'
-p center_of('x') == 'x'
+def twice(num)
+
+  # if num isn't a double number, return num * 2
+  # if it is a double number, return num
+  return num if double_number?(num)
+  num * 2
+end
+
+p twice(37) == 74
+p twice(44) == 44
+p twice(334433) == 668866
+p twice(444) == 888
+p twice(107) == 214
+p twice(103103) == 103103
+p twice(3333) == 3333
+p twice(7676) == 7676
+p twice(123_456_789_123_456_789) == 123_456_789_123_456_789
+p twice(5) == 10
+
+# Note: underscores are used for clarity above. Ruby lets you use underscores when writing long numbers; however, it does not print the underscores when printing long numbers. Don't be alarmed if you don't see the underscores when running your tests.
+
